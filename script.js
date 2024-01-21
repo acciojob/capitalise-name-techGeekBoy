6029418b09c6f81b9c7072ab7e1e843468b43450
techGeekBoy <148032840+techGeekBoy@users.noreply.github.com>
2024-01-21 11:56:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let capitilized = function(sent){
 	let str = sent.split(" ");
 	for(let i=0; i<str.length; i++){
-		str[i]=str[i][0].toUpperCase()+str[i].slice(1);
+		str[i]=str[i][0].toUpperCase()+str[i].slice(1).toLowerCase();
 	}
 	return str.join(" ");
 }
