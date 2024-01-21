9af8fbc5920369970f2d092922bdc780dbc1bdc7
techGeekBoy <148032840+techGeekBoy@users.noreply.github.com>
2024-01-21 12:17:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let capitalize = function(sent){
     let words = sent.split(" ");
     for(let i = 0; i < words.length; i++){
-        words[i] = words[i][0].toUpperCase() + words[i].slice(1).toLowerCase();
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
     }
     return words.join(" ");
 }
